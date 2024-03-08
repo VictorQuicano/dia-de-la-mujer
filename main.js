fb2a7b81fc07b1fc9af077716c826d837ff60228
VictorQuicano <alejandroxd25k@gmail.com>
2024-03-08 14:03:39 -0500

    Anadidos cambios para letras y mensajes

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-onload = () =>{
-        document.body.classList.remove("container");
+const queryString = window.location.search;
+const params = decodeURIComponent(queryString.substring(1));
+const name = params.split("=")[1];
+console.log(name);
+
+onload = () => {
+  document.body.classList.remove("container");
+  document.getElementById("name").innerHTML = name;
 };
